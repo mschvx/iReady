73daf384d7ccabdc402f857960e9b41ad30377f8
VEN <184076588+frougsk@users.noreply.github.com>
2025-10-21 08:10:26 +0000

    cryptop eme

--- Landing/server/index.ts
+++ Landing/server/index.ts
@@ -1,8 +1,16 @@
 import { webcrypto } from "crypto";
-(globalThis as any).crypto = webcrypto;
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+
+if (!globalThis.crypto) {
+  Object.defineProperty(globalThis, "crypto", {
+    value: webcrypto,
+    configurable: false,
+    enumerable: true,
+    writable: false,
+  });
+}
 
 const app = express();
 app.use(express.json());
