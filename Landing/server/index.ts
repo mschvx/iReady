50c4aaacf78eaf777797406b6ed484301c33655f
VEN <184076588+frougsk@users.noreply.github.com>
2025-10-21 08:06:20 +0000

    crypto

--- Landing/server/index.ts
+++ Landing/server/index.ts
@@ -1,3 +1,5 @@
+import { webcrypto } from "crypto";
+(globalThis as any).crypto = webcrypto;
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
